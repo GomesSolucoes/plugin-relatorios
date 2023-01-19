0c9db25c4fa2f0666a6c75a3cb09606fbcbefdcd
paulo.gomes <30624927+Paulo4025@users.noreply.github.com>
2023-01-19 01:06:00 -0300

    feature: Reconstrução do projeto para a utilização do microfrontends

--- plugin-relatorios-frontend/src/app/pages/relatorios/relatorios.component.ts
+++ plugin-relatorios-frontend/src/app/pages/relatorios/relatorios.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { MensagensPTBRCore } from "app/plugins/plugin-core/plugin-core-frontend/src/app/i18n/MensagensPTBRCore";
-import { UtilCore } from "app/plugins/plugin-core/plugin-core-frontend/src/app/util/util-core";
+import { UtilCore } from "plugins/plugin-core/plugin-core-frontend/src/app/util/util-core";
 import { MensagensPTBRRelatorios } from "../../i18n/MensagensPTBRRelatorios";
 @Component({
   selector: "app-relatorios",
--- plugin-relatorios-frontend/src/app/plugin-relatorios.module.ts
+++ plugin-relatorios-frontend/src/app/plugin-relatorios.module.ts
@@ -2,51 +2,28 @@ import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatNativeDateModule } from "@angular/material/core";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { RouterModule } from "@angular/router";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { NgxMaskModule } from "ngx-mask";
-import { MatSelectModule } from "@angular/material/select";
-import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-import { MatExpansionModule } from "@angular/material/expansion";
 import { CdkAccordionModule } from "@angular/cdk/accordion";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatTableModule } from "@angular/material/table";
 import { CdkTableModule } from "@angular/cdk/table";
-import { MatTabsModule } from "@angular/material/tabs";
-import { MensagensPTBRRelatorios } from "./plugin-relatorios-frontend/src/app/i18n/MensagensPTBRRelatorios";
-import { ServiceConfigRelatorios } from "./plugin-relatorios-frontend/src/app/service/ServiceConfigRelatorios";
-import { RelatoriosComponent } from "./plugin-relatorios-frontend/src/app/pages/relatorios/relatorios.component";
-import { UtilConstantesRelatorios } from "./plugin-relatorios-frontend/src/app/util/util-constantes-relatorios";
-import { UtilRelatorios } from "./plugin-relatorios-frontend/src/app/util/util-relatorio";
-import { FechamentoLivroCaixaComponent } from "./plugin-relatorios-frontend/src/app/pages/relatorios/fechamento-livro-caixa/fechamento-livro-caixa.component";
+import { MensagensPTBRRelatorios } from "./i18n/MensagensPTBRRelatorios";
+import { FechamentoLivroCaixaComponent } from "./pages/relatorios/fechamento-livro-caixa/fechamento-livro-caixa.component";
+import { RelatoriosComponent } from "./pages/relatorios/relatorios.component";
+import { ServiceConfigRelatorios } from "./service/ServiceConfigRelatorios";
+import { UtilConstantesRelatorios } from "./util/util-constantes-relatorios";
+import { UtilRelatorios } from "./util/util-relatorio";
+import { MaterialModule } from "app/modules/material.module";
+import { NgxMaskModule } from "ngx-mask";
 @NgModule({
   imports: [
     NgxMaskModule.forRoot(),
     CommonModule,
     RouterModule,
     HttpClientModule,
-    MatSnackBarModule,
     FormsModule,
     ReactiveFormsModule,
-    MatSnackBarModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSelectModule,
-    MatSlideToggleModule,
-    MatExpansionModule,
     CdkAccordionModule,
-    MatPaginatorModule,
-    MatTableModule,
     CdkTableModule,
-    MatTabsModule,
+    MaterialModule
   ],
   declarations: [FechamentoLivroCaixaComponent, RelatoriosComponent,FechamentoLivroCaixaComponent],
   exports: [RelatoriosComponent, FechamentoLivroCaixaComponent],
